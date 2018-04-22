ff29339e6f89625e6aaa08ce8121ba302950d59b
valenciafu <valenciafu@gmail.com>
2018-04-22 17:04:37 -0400

    Euclidean

--- distances.py
+++ distances.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def hamming(string1, string2):
 	diffs = 0
 	for c1, c2 in zip(string1, string2):
@@ -5,8 +7,20 @@ def hamming(string1, string2):
 			diffs += 1
 	return diffs
 
+
 def euclidean(string1, string2):
-  return 0
+	dist = 0
+	for c1, c2 in zip(string1, string2):
+		dist += np.linalg.norm(c1, c2)
+	return dist
+
 
 def manhattan(string1, string2):
-  return 0
+	return 0
+
+if __name__ == "__main__":
+	s1 = "Hello"
+	s2 = "Hullo"
+
+	res = hamming(s1, s2)
+	print(res)
