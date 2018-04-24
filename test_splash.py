27d09bd245ae1ef705236ef62a8c4387dd00e705
valenciafu <valenciafu@gmail.com>
2018-04-24 11:00:13 -0400

    Styling

--- test_splash.py
+++ test_splash.py
@@ -3,13 +3,13 @@ from distances import hamming, euclidean, manhattan
 
 app = Flask(__name__, static_folder='static')
 
-app.secret_key = 'You Will Never Guess'
-
 @app.route('/', methods=['GET', 'POST'])
 def splash():
-	session['sentence1'] = request.args.get('sentence1')
-	session['sentence2'] = request.args.get('sentence2')
-	return render_template("index.html", sentence1=session['sentence1'], sentence2=session['sentence2'])
+	if request.method == 'POST':
+		s1 = request.args.get('sentence1')
+		s2 = request.args.get('sentence2')
+		return render_template("index.html", s1=s1, s2=s2)
+	return render_template("index.html")
 
 @app.route('/similarity')
 def research():
