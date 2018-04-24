5af6257dc59e40fedb77c0d8768074cafef3cf75
valenciafu <valenciafu@gmail.com>
2018-04-24 16:47:49 -0400

    Revert "Make distances ratios"
    This reverts commit 2257d4701156fb9c37e7bd406e8377eb1a86c3de.

--- distances.py
+++ distances.py
@@ -3,40 +3,27 @@ import numpy as np
 
 def hamming(string1, string2):
 	diffs = 0
-	if len(string1) != len(string2):
-		diffs += abs(len(string1) - len(string2))
-
 	for c1, c2 in zip(string1, string2):
 		if c1 != c2:
 			diffs += 1
-
-	return diffs / max(len(string1), len(string2))
+	return diffs
 
 
 def euclidean(string1, string2):
 	dist = 0
 	temp1 = [ord(char) for char in string1]
 	temp2 = [ord(char) for char in string2]
-
-	res = math.sqrt(sum((t1 - t2) ** 2 for t1, t2 in zip(temp1, temp2)))
-	max_distance = 26 * max(len(string1), len(string2))
-
-	return res / max_distance
+	return math.sqrt(sum((t1 - t2) ** 2 for t1, t2 in zip(temp1, temp2)))
 
 
 def manhattan(string1, string2):
 	temp1 = [ord(char) for char in string1]
 	temp2 = [ord(char) for char in string2]
-
-	res = sum(abs(t1 - t2) for t1, t2 in zip(temp1, temp2))
-	max_distance = 26 * max(len(string1), len(string2))
-
-	return res / max_distance
-
+	return sum(abs(t1 - t2) for t1, t2 in zip(temp1, temp2))
 
 if __name__ == "__main__":
-	s1 = "Hello"
-	s2 = "Henlo"
+	s1 = "Helno"
+	s2 = "Hullo"
 
 	res1 = hamming(s1, s2)
 	print(res1)
