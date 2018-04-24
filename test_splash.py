0232d5619335317eb14b314688a4500ae9a00b8d
valenciafu <valenciafu@gmail.com>
2018-04-24 16:21:52 -0400

    Fix form

--- test_splash.py
+++ test_splash.py
@@ -5,11 +5,15 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/', methods=['GET', 'POST'])
 def splash():
-	if request.method == 'POST':
-		s1 = request.args.get('sentence1')
-		s2 = request.args.get('sentence2')
-		return render_template("index.html", s1=s1, s2=s2)
-	return render_template("index.html")
+	# if request.method == 'POST':
+	# 	s1 = request.args.get('sentence1')
+	# 	s2 = request.args.get('sentence2')
+	# 	return render_template("index.html", s1=s1, s2=s2)
+	# return render_template("index.html")
+
+	sentence1 = request.args.get('sentence1')
+	sentence2 = request.args.get('sentence2')
+	return render_template("index.html", sentence1=sentence1, sentence2=sentence2)
 
 if __name__ == "__main__":
   	app.run(debug=True)
