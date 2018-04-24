2a617a86319a31ea54b9af00b0aca22370977a5d
arun <arun.kirubarajan@gmail.com>
2018-04-24 16:15:29 -0400

    Add NLP comments and window expansion

--- nlp.py
+++ nlp.py
@@ -13,7 +13,7 @@ def tokenize(string):
   tokens = [token for token in tokens if not token in english_stop_words]
   return tokens
 
-# Splits text into vectorize-able sentences
+# Splits text into vectorize-able sentences 
 def get_sentences(string):
   sentences = [tokenize(sentence) for sentence in string.split(".")]
   return sentences
@@ -21,24 +21,29 @@ def get_sentences(string):
 # Generates and trains word2vec model
 def get_model(data):
   sentences = get_sentences(data)
-  model = Word2Vec(sentences, min_count=1)
+  model = Word2Vec(sentences, min_count=1, window=15)
   return model
 
+# Generates and trains word2vec model
 def average_vector(vectors):
   return sum(vectors) / len(vectors)
 
+# scales sigmoid function to work better with inputs within range [-1, 1]
 def scaled_sigmoid(n):
-  return 1 / (1 + math.exp(-n))
+  return 1 / (1 + math.exp(-(5000 * n)))
 
+# Calculates similary using cosine distance
 def similarity(string1, string2):
   with open('corpus.txt', 'r') as f:
     data = f.read().replace('\n', '')
   model = get_model(data)
 
-  string1_token_embeddings = [model.wv(token) for token in tokenize(string1)]
-  string2_token_embeddings = [model.wv(token) for token in tokenize(string1)]
+  string1_token_embeddings = [model[token] for token in tokenize(string1) if token in model]
+  string2_token_embeddings = [model[token] for token in tokenize(string2) if token in model]
 
   string1_embedding = average_vector(string1_token_embeddings)
   string2_embedding = average_vector(string2_token_embeddings)
 
-  return scaled_sigmoid(distance.cosine(string1_embedding, string2_embedding))
+  return distance.cosine(string1_embedding, string2_embedding)
+
+print(similarity("Afghanistan needs work", "America needs work"))
