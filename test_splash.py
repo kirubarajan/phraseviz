772778b56489ff5e2888699a69fc55c0f8de73dd
valenciafu <valenciafu@gmail.com>
2018-04-24 11:15:56 -0400

    Delete route

--- test_splash.py
+++ test_splash.py
@@ -11,9 +11,5 @@ def splash():
 		return render_template("index.html", s1=s1, s2=s2)
 	return render_template("index.html")
 
-@app.route('/similarity')
-def research():
-  	return render_template("similarity.html", sentence1=session['sentence1'], sentence2=session['sentence2'])
-
 if __name__ == "__main__":
   	app.run(debug=True)
